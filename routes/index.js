23ea06d60c41dd16d3a7da6c8b334b7126d3dc39
Jamie Bond <BritishCounterpart@gmail.com>
2020-12-12 15:34:53 +0000

    Created most recent petition card

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const Auth = require('../middleware/Auth');
+const Petitions = require('../models/Petitions');
 
 // home page
-router.get('/', Auth.optionalAuthenticated, function(req, res, next) {
-  res.render('index', {user: req.user});
+router.get('/', Auth.optionalAuthenticated, async function(req, res, next) {
+    let petitions = await Petitions.find({}).sort({createdOn: 'desc'}).limit(5).exec();
+    res.render('index', {
+        user: req.user,
+        petitions: petitions
+    });
 });
 
 module.exports = router;
